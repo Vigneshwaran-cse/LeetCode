9c9ebe90136b9f953d56b44eb1a3e9b265be9529
Vigneshwaran-cse <vigneshwaran2006csc@gmail.com>
2026-08-21 08:55:38 +0530

    Time: 6 ms (29.14%) | Memory: 43.5 MB (60.22%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations); // sort ascending
+        int n = citations.length;
+        int h = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int candidate = Math.min(citations[i], n - i);
+            h = Math.max(h, candidate);
+        }
+        
+        return h;
+    }
+}
