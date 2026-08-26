0f16377d4b5988460471071e390d4810f5c8ec8c
Vigneshwaran-cse <vigneshwaran2006csc@gmail.com>
2026-08-26 08:27:42 +0530

    Time: 2 ms (99.90%) | Memory: 46.3 MB (96.63%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,7 +1,19 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        String doubled = s + s;
-        // Check if s exists in doubled string excluding first and last char
-        return doubled.substring(1, doubled.length() - 1).contains(s);
+       int length = s.length();
+       int upto = (int) Math.ceil(Math.sqrt(length));
+       for(int i = 1; i <= upto; ++i){
+        if(length%i == 0){
+            if(i != length && check(s, s.substring(0, i)))
+                return true;
+            int j = length/i;
+            if(j != 1 && j != length && check(s, s.substring(0, j)))
+                return true;
+        }
+       }
+       return false;
+    }
+    public boolean check(String s, String subString){
+        return s.endsWith(subString) && subString.repeat(s.length()/subString.length()).equals(s);
     }
 }
