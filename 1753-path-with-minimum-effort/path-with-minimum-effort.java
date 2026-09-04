c13b97a55f8c3b509e8aa45f689d5522e766186d
Vigneshwaran-cse <vigneshwaran2006csc@gmail.com>
2026-09-04 15:16:25 +0530

    Time: 40 ms (75.83%) | Memory: 48 MB (43.32%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,31 +1,44 @@
 import java.util.*;
-
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int rows = heights.length, cols = heights[0].length;
-        int[][] effort = new int[rows][cols];
-        for (int[] row : effort) Arrays.fill(row, Integer.MAX_VALUE);
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
-        pq.offer(new int[]{0, 0, 0}); // {effort, row, col}
-        effort[0][0] = 0;
-        
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int e = cur[0], r = cur[1], c = cur[2];
-            
-            if (r == rows-1 && c == cols-1) return e;
-            
-            for (int[] d : dirs) {
-                int nr = r + d[0], nc = c + d[1];
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
-                    int nextEffort = Math.max(e, Math.abs(heights[nr][nc] - heights[r][c]));
-                    if (nextEffort < effort[nr][nc]) {
-                        effort[nr][nc] = nextEffort;
-                        pq.offer(new int[]{nextEffort, nr, nc});
-                    }
+        int n = heights.length;
+        int m = heights[0].length;
+        int[][] dist = new int [n][m];
+
+        for(int[] row : dist){
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->Integer.compare(a[0], b[0]));
+        dist[0][0] = 0;
+        pq.offer(new int[]{0, 0, 0});
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int effort = curr[0];
+            int r = curr[1];
+            int c = curr[2];
+
+            if(effort > dist[r][c]){
+                continue;
+            }
+            if(r == n - 1 && c == m - 1){
+                return effort;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) {
+                    continue;
+                }
+                int edgeCost = Math.abs(heights[r][c] - heights[nr][nc]);
+                int newEffort = Math.max(effort, edgeCost);
+                if (newEffort < dist[nr][nc]) {
+                    dist[nr][nc] = newEffort;
+
+                    pq.offer(new int[]{newEffort, nr, nc});
                 }
             }
         }
